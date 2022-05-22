53b3b38230014d5e8b5d0c1467261b384661cba1
Willian <willian_assufialbanez@hotmail.com>
2022-05-22 14:50:27 -0300

    Adicionando senhas

--- Conta_Clientes.py
+++ Conta_Clientes.py
@@ -9,4 +9,21 @@ class Base_Dados:
         id = dados.get(self.idconta)
         return id
     
+    def senha(self, valor):
+        id = valor
+        dados = {100 : 1111, 101 : 2222, 102 : 3333, 103 : 4444, 104 : 5555}
+        senha = dados[id]
+        entrada = 0
+        while(entrada != senha):
+            entrada = int(input("\nDigite seu PIN de 4 dígitos: "))
+            if(entrada == senha):
+                break
+            else: 
+                print("\nPIN incorreto! Tente Novamente...")
+            
+
+        
     
+
+
+        
--- Projeto_Bancario.py
+++ Projeto_Bancario.py
@@ -1,25 +1,27 @@
 from Conta_Atributos import Conta
 from Conta_Clientes import Base_Dados
 
-base = Base_Dados(1)
-conta = Conta(123, "Willian", 100.00, 1000.00)
+contas = Base_Dados(0)
+funções = Conta(0, "", 100.00, 1000.00)
 
 op = 4
+id = int(input("\nDigite o número da sua conta: "))
+contas.senha(id)
+
 
 while(op != 0):
     
-    id = int(input("Digite o número da sua conta:\n"))
-    print("\n{} Seja Bem vindo(a) ao SystemInvest\nEscolha a operação\n".format(base.cliente(id)))
+    print("\n{} Seja Bem vindo(a) ao SystemInvest\nEscolha a operação\n".format(contas.cliente(id)))
     op = int(input("(1)Sacar\n(2)Depositar\n(3)Extrato\n(0)Sair\n\n"))
 
     if(op == 1):
         num = float(input("\nDigite o valor que deseja sacar: R$"))
-        conta.saque(num)
+        funções.saque(num)
     elif(op == 2):
         num = float(input("\nDigite o valor que deseja depositar: R$"))
-        conta.depositar(num)
+        funções.depositar(num)
     elif(op == 3):
-        conta.extrato()
+        funções.extrato()
     elif(op == 0):
         print("\nSaindo...")
         exit()
